2a9083f8f5633c1e5f7705a3fd3fd6e49b4b77cc
radiocontrolled <radiocontrolled@gmail.com>
2014-03-02 17:50:38 +0000

    update tooltip.js after using js.lint

--- vanillaTooltip/tooltip.js
+++ vanillaTooltip/tooltip.js
@@ -1,20 +1,21 @@
+
 var input = document.getElementById("input");
 var tooltip = null;
 var tooltipContents = "Hi, I'm a tooltip.";
 var box = document.createElement("div");
 
-box.setAttribute("id","focus");
+box.setAttribute("id", "focus");
 box.style.display = "none";
 
-function showTooltip(){
-	box.style.display = "inline";
-	tooltip = document.getElementById("focus");
-	box.innerHTML = tooltipContents;
-	input.parentNode.insertBefore(box,input.nextSibling);
+function showTooltip() {
+    box.style.display = "inline";
+    tooltip = document.getElementById("focus");
+    box.innerHTML = tooltipContents;
+    input.parentNode.insertBefore(box, input.nextSibling);
 }
 
-function hideToolTip(){
-	box.remove();
+function hideToolTip() {
+    box.remove();
 }
 
 //mouse event listener 
